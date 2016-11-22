fb06983aca8fdcbbb6dd6437b4eb4cdb760d8358
Jonathan Turner <jturner@mozilla.com>
2016-11-22 14:50:19 -0800

    Add tests for hover and completion

--- src/test/mod.rs
+++ src/test/mod.rs
@@ -78,7 +78,7 @@ fn test_abs_path() {
 }
 
 #[test]
-fn test_simple_goto_def() {
+fn test_goto_def() {
     let _cr = CwdRestorer::new();
 
     init_env("hello");
@@ -104,13 +104,75 @@ fn test_simple_goto_def() {
                                        ExpectedMessage::new(None).expect_contains("diagnosticsBegin"),
                                        ExpectedMessage::new(None).expect_contains("diagnosticsEnd")]);
 
-    // Goto def.
     assert_eq!(ls_server::LsService::handle_message(server.clone()),
                ls_server::ServerStateChange::Continue);
     // TODO structural checking of result, rather than looking for a string - src(&source_file_path, 12, "world")
     expect_messages(results.clone(), &[ExpectedMessage::new(Some(42)).expect_contains("\"start\":{\"line\":11,\"character\":8}")]);
 }
 
+#[test]
+fn test_hover() {
+    let _cr = CwdRestorer::new();
+
+    init_env("hello");
+    let mut cache = types::Cache::new(Path::new("."));
+
+    let source_file_path = Path::new("src").join("main.rs");
+
+    let root_path = format!("{}", serde_json::to_string(&cache.abs_path(Path::new(".")))
+                                      .expect("couldn't convert path to JSON"));
+    let url = Url::from_file_path(cache.abs_path(&source_file_path)).expect("couldn't convert file path to URL");
+    let text_doc = format!("{{\"uri\":{}}}", serde_json::to_string(&url.as_str().to_owned())
+                                                 .expect("couldn't convert path to JSON"));
+    let messages = vec![Message::new("initialize", vec![("processId", "0".to_owned()),
+                                                        ("rootPath", root_path)]),
+                        Message::new("textDocument/hover",
+                                     vec![("textDocument", text_doc),
+                                          ("position", cache.mk_ls_position(src(&source_file_path, 13, "world")))])];
+    let (server, results) = mock_lsp_server(messages);
+    // Initialise and build.
+    assert_eq!(ls_server::LsService::handle_message(server.clone()),
+               ls_server::ServerStateChange::Continue);
+    expect_messages(results.clone(), &[ExpectedMessage::new(Some(42)).expect_contains("capabilities"),
+                                       ExpectedMessage::new(None).expect_contains("diagnosticsBegin"),
+                                       ExpectedMessage::new(None).expect_contains("diagnosticsEnd")]);
+
+    assert_eq!(ls_server::LsService::handle_message(server.clone()),
+               ls_server::ServerStateChange::Continue);
+    expect_messages(results.clone(), &[ExpectedMessage::new(Some(42)).expect_contains("[{\"language\":\"rust\",\"value\":\"&str\"}]")]);
+}
+
+#[test]
+fn test_completion() {
+    let _cr = CwdRestorer::new();
+
+    init_env("hello");
+    let mut cache = types::Cache::new(Path::new("."));
+
+    let source_file_path = Path::new("src").join("main.rs");
+
+    let root_path = format!("{}", serde_json::to_string(&cache.abs_path(Path::new(".")))
+                                      .expect("couldn't convert path to JSON"));
+    let url = Url::from_file_path(cache.abs_path(&source_file_path)).expect("couldn't convert file path to URL");
+    let text_doc = format!("{{\"uri\":{}}}", serde_json::to_string(&url.as_str().to_owned())
+                                                 .expect("couldn't convert path to JSON"));
+    let messages = vec![Message::new("initialize", vec![("processId", "0".to_owned()),
+                                                        ("rootPath", root_path)]),
+                        Message::new("textDocument/completion",
+                                     vec![("textDocument", text_doc),
+                                          ("position", cache.mk_ls_position(src(&source_file_path, 13, "rld")))])];
+    let (server, results) = mock_lsp_server(messages);
+    // Initialise and build.
+    assert_eq!(ls_server::LsService::handle_message(server.clone()),
+               ls_server::ServerStateChange::Continue);
+    expect_messages(results.clone(), &[ExpectedMessage::new(Some(42)).expect_contains("capabilities"),
+                                       ExpectedMessage::new(None).expect_contains("diagnosticsBegin"),
+                                       ExpectedMessage::new(None).expect_contains("diagnosticsEnd")]);
+
+    assert_eq!(ls_server::LsService::handle_message(server.clone()),
+               ls_server::ServerStateChange::Continue);
+    expect_messages(results.clone(), &[ExpectedMessage::new(Some(42)).expect_contains("[{\"label\":\"world\",\"detail\":\"let world = \\\"world\\\";\"}]")]);
+}
 
 #[test]
 fn test_parse_error_on_malformed_input() {
