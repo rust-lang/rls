f9f505f762e1bf1d2106b3a96bbd6cbd6a5ae428
Igor Matuszewski <Xanewok@gmail.com>
2017-04-08 14:17:53 +0200

    simplified build request logic

--- src/build.rs
+++ src/build.rs
@@ -130,15 +130,14 @@ impl BuildQueue {
         // If there is a change in the project directory, then we can forget any
         // pending build and start straight with this new build.
         {
-            let mut reset = false;
             let mut prev_build_dir = self.build_dir.lock().unwrap();
-            if let Some(ref prev_build_dir) = *prev_build_dir {
-                if prev_build_dir != build_dir {
-                    reset = true;
-                }
-            }
 
-            if reset || prev_build_dir.is_none() {
+            let reset = match *prev_build_dir {
+                Some(ref dir) if dir == build_dir => false,
+                _ => true
+            };
+
+            if reset {
                 *prev_build_dir = Some(build_dir.to_owned());
                 self.cancel_pending();
 
