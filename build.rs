c3d99adb4d4a38155c8a4af3c0999f9f63dd54ae
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2017-05-11 08:25:07 +0900

    Remove Ignore in /build.rs

--- build.rs
+++ build.rs
@@ -12,21 +12,10 @@
 // [rustup]: https://github.com/rust-lang-nursery/rustup.rs/tree/a1c6be19add9c99f8d8868ec384aa76057fe7f70
 
 use std::env;
-use std::error::Error;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
-
-struct Ignore;
-
-impl<E> From<E> for Ignore
-    where E: Error
-{
-    fn from(_: E) -> Ignore {
-        Ignore
-    }
-}
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
@@ -41,24 +30,26 @@ fn main() {
 // (git not installed or if this is not a git repository) just return an empty string.
 fn commit_info() -> String {
     match (commit_hash(), commit_date()) {
-        (Ok(hash), Ok(date)) => format!(" ({} {})", hash.trim_right(), date),
+        (Some(hash), Some(date)) => format!(" ({} {})", hash.trim_right(), date),
         _ => String::new(),
     }
 }
 
-fn commit_hash() -> Result<String, Ignore> {
-    Ok(try!(String::from_utf8(try!(Command::new("git")
-                                       .args(&["rev-parse", "--short", "HEAD"])
-                                       .output())
-                                  .stdout)))
+fn commit_hash() -> Option<String> {
+    Command::new("git")
+        .args(&["rev-parse", "--short", "HEAD"])
+        .output()
+        .ok()
+        .and_then(|r| String::from_utf8(r.stdout).ok())
 }
 
-fn commit_date() -> Result<String, Ignore> {
-    Ok(try!(String::from_utf8(try!(Command::new("git")
-                                       .args(&["log",
-                                               "-1",
-                                               "--date=short",
-                                               "--pretty=format:%cd"])
-                                       .output())
-                                  .stdout)))
+fn commit_date() -> Option<String> {
+    Command::new("git")
+        .args(&["log",
+                "-1",
+                "--date=short",
+                "--pretty=format:%cd"])
+        .output()
+        .ok()
+        .and_then(|r| String::from_utf8(r.stdout).ok())
 }
