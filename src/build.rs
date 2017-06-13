0d0f3baad02d65d96befbb90e77bf8a326dd14f5
Nick Cameron <ncameron@mozilla.com>
2017-06-13 15:14:26 +1200

    Do the FORCE_JSON stuff with comments so we can compile

--- src/build.rs
+++ src/build.rs
@@ -48,10 +48,6 @@ use std::sync::mpsc::{channel, Sender};
 use std::thread;
 use std::time::Duration;
 
-// If true, this forces the compiler to pass all data via the disk instead of
-// in memory. This is slower but can be useful for debugging and making backwards
-// incompatible changes.
-const FORCE_JSON: bool = true;
 
 /// Manages builds.
 ///
@@ -568,29 +564,32 @@ impl BuildQueue {
                 let analysis = self.analysis.clone();
 
                 result.after_analysis.callback = Box::new(move |state| {
-                    if FORCE_JSON {
-                        save::process_crate(state.tcx.unwrap(),
-                                            state.expanded_crate.unwrap(),
-                                            state.analysis.unwrap(),
-                                            state.crate_name.unwrap(),
-                                            save::DumpHandler::new(save::Format::Json,
-                                                                   state.out_dir,
-                                                                   state.crate_name.unwrap()));
-                    } else {
-                        save::process_crate(state.tcx.unwrap(),
-                                            state.expanded_crate.unwrap(),
-                                            state.analysis.unwrap(),
-                                            state.crate_name.unwrap(),
-                                            CallbackHandler {
-                                                callback: &mut |a| {
-                                                    let mut analysis = analysis.lock().unwrap();
-                                                    let a = unsafe {
-                                                        ::std::mem::transmute(a.clone())
-                                                    };
-                                                    *analysis = Some(a);
-                                                }
-                                            });
-                    }
+                    // There are two ways to move the data from rustc to the RLS, either
+                    // directly or by serialising and deserialising. We only want to do 
+                    // the latter when there are compatibility issues between crates.
+
+                    // This version passes via JSON, it is more easily backwards compatible.
+                    save::process_crate(state.tcx.unwrap(),
+                                        state.expanded_crate.unwrap(),
+                                        state.analysis.unwrap(),
+                                        state.crate_name.unwrap(),
+                                        save::DumpHandler::new(save::Format::Json,
+                                                               state.out_dir,
+                                                               state.crate_name.unwrap()));
+                    // This version passes directly, it is more efficient.
+                    // save::process_crate(state.tcx.unwrap(),
+                    //                     state.expanded_crate.unwrap(),
+                    //                     state.analysis.unwrap(),
+                    //                     state.crate_name.unwrap(),
+                    //                     CallbackHandler {
+                    //                         callback: &mut |a| {
+                    //                             let mut analysis = analysis.lock().unwrap();
+                    //                             let a = unsafe {
+                    //                                 ::std::mem::transmute(a.clone())
+                    //                             };
+                    //                             *analysis = Some(a);
+                    //                         }
+                    //                     });
                 });
                 result.after_analysis.run_callback_on_error = true;
                 result.make_glob_map = rustc_resolve::MakeGlobMap::Yes;
