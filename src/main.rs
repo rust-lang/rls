8757b02537dc5332ae66ce3ac1d1df0bc1e44fd9
Nick Cameron <ncameron@mozilla.com>
2017-06-14 14:07:28 +1200

    Use rustfmt-nightly

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ extern crate rls_analysis as analysis;
 extern crate rls_vfs as vfs;
 extern crate rls_span as span;
 extern crate rls_data as data;
-extern crate rustfmt;
+extern crate rustfmt_nightly as rustfmt;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
