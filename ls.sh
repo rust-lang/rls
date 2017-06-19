7d16fd3af9f3b16ef609a49380e3f23368f6579e
Nick Cameron <nrc@ncameron.org>
2017-06-20 10:06:27 +1200

    Delete ls.sh

--- ls.sh
+++
@@ -1,6 +0,0 @@
-export PATH="$PWD/target/debug:$PATH"
-#export RUSTC="/home/ncameron/rust/x86_64-unknown-linux-gnu/stage2/bin/rustc"
-#export SYS_ROOT="/home/ncameron/rust/x86_64-unknown-linux-gnu/stage2"
-#export SYS_ROOT="/usr/local"
-export RUST_BACKTRACE=1
-cargo build && code
