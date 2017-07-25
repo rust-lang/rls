48221907ac3f1260f3a9062dee2b4cde432db68a
Nick Cameron <ncameron@mozilla.com>
2017-07-25 16:34:35 +1200

    Only include public data in dep crate save-analysis

--- src/build/cargo.rs
+++ src/build/cargo.rs
@@ -128,7 +128,8 @@ fn run_cargo(exec: RlsExecutor, rls_config: Arc<Mutex<Config>>, build_dir: PathB
 
     let mut restore_env = Environment::push(&env);
     // FIXME use serialize for this rather than writing in out by hand.
-    restore_env.push_var("RUST_SAVE_ANALYSIS_CONFIG", &Some(OsString::from(r#"{"output_file": null, "full_docs": false, "pub_only": false, "signatures": false, "borrow_data": false}"#)));
+    let save_config = r#"{"output_file": null, "full_docs": false, "pub_only": true, "signatures": false, "borrow_data": false}"#;
+    restore_env.push_var("RUST_SAVE_ANALYSIS_CONFIG", &Some(OsString::from(save_config)));
 
     compile_with_exec(&ws, &compile_opts, Arc::new(exec)).expect("could not run cargo");
 }
