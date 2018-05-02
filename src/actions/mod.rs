af675581c36c9db589d009e7aace5c9b2aa0becb
Igor Matuszewski <Xanewok@gmail.com>
2018-05-02 23:51:02 +0200

    Split diagnostics into separate module

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -57,6 +57,7 @@ pub mod post_build;
 pub mod requests;
 pub mod notifications;
 pub mod progress;
+pub mod diagnostics;
 
 /// Persistent context shared across all requests and notifications.
 pub enum ActionContext {
