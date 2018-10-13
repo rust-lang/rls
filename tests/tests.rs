95de53a7bc12b4ae3fece13d5d8227cc04814925
Matthias Krüger <matthias.krueger@famsik.de>
2018-10-14 00:25:34 +0200

    the tool_lints feature is stable in rust nightly now.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,3 @@
-#![feature(tool_lints)]
-
 // Copyright 2016 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
