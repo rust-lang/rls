163bd3bbb4de49051f66d974a492fba212bb791f
Alexander Regueiro <alexreg@me.com>
2019-02-04 14:55:15 +0100

    Removed copyright notices.

--- rls/src/build/cargo_plan.rs
+++ rls/src/build/cargo_plan.rs
@@ -1,13 +1,3 @@
-// Copyright 2017 The Rust Project Developers. See the COPYRIGHT
-// file at the top-level directory of this distribution and at
-// http://rust-lang.org/COPYRIGHT.
-//
-// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
-// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
-// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
-// option. This file may not be copied, modified, or distributed
-// except according to those terms.
-
 //! This contains a build plan that is created during the Cargo build routine
 //! and stored afterwards, which can be later queried, given a list of dirty
 //! files, to retrieve a queue of compiler calls to be invoked (including
