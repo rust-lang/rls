fe6e31ffa76511a42db97c1e5a23998a78b868c7
Igor Matuszewski <Xanewok@gmail.com>
2019-02-06 15:58:11 +0100

    Move rls-blacklist crate
    Based on
    https://github.com/rust-dev-tools/rls-blacklist/commit/f6419ca8beaaaffbec8bf8e83e0b246900872819

---
+++ rls-blacklist/src/lib.rs
@@ -0,0 +1,15 @@
+pub const CRATE_BLACKLIST: [&'static str; 13] = [
+    "cocoa",
+    "gleam",
+    "glium",
+    "idna",
+    "libc",
+    "openssl",
+    "rustc_serialize",
+    "serde",
+    "serde_json",
+    "typenum",
+    "unicode_normalization",
+    "unicode_segmentation",
+    "winapi",
+];
